9b0dcc781d3202db7f1fff7270c3d3523f64703d
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-04-02 00:49:06 -0400

    fixed blog page

--- pages/lavora/index.tsx
+++ pages/lavora/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Row } from "../../styles/grid";
 import Lavora from "../../components/Lavora";
-import { BsTypeH1 } from "react-icons/bs";
 
 const LavoraConNoi = () => {
   return (
